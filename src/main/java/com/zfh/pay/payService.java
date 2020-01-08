5689a58b9526f11f6e6b9ef23625d43709be3c23
zfh <zhangfh9@foxmail.com>
2020-01-08 22:29:42 +0800

    增加支付

---
+++ src/main/java/com/zfh/pay/payService.java
@@ -0,0 +1,7 @@
+package com.zfh.pay;
+
+public interface payService {
+    public void add(Double money);
+
+
+}
---
+++ src/main/java/com/zfh/pay/payServiceImpl.java
@@ -0,0 +1,8 @@
+package com.zfh.pay;
+
+public class payServiceImpl implements payService {
+    @Override
+    public void add(Double money) {
+        System.out.println("添加支付");
+    }
+}
